c64e6262afc8261d7d37457859e86cf4b99acb46
MaksimSimdynkin <nin202@mail.ru>
2026-01-26 14:42:53 +0400

    Init commit

---
+++ src/test/java/ru/myproject/reservation/ReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.myproject.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
